71a6e8e10c107732a809021d3da2738d41f1215b
Santhoshkumar V <shanvsanthosh@gmail.com>
2025-07-19 11:38:30 +0530

    Initial commit

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 terraform {
+  required_version = ">= 1.6.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.40.0, < 6.0.0"
     }
   }
-
-  required_version = ">= 1.6.0"
 }
+
+
 
 provider "aws" {
   region = var.aws_region
@@ -15,15 +16,15 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "6.0.1"
+  version = "5.1.2"  # ✅ Tested version that works with AWS provider <6.0.0
 
-  name               = "flask-scalable-vpc"
-  cidr               = "10.0.0.0/16"
-  azs                = ["${var.aws_region}a", "${var.aws_region}b"]
-  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets    = ["10.0.3.0/24", "10.0.4.0/24"]
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  name                 = "flask-scalable-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = ["${var.aws_region}a", "${var.aws_region}b"]
+  public_subnets       = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets      = ["10.0.3.0/24", "10.0.4.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
 
   tags = {
     Terraform   = "true"
@@ -32,8 +33,9 @@ module "vpc" {
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.37.2" # you can keep this or latest 20.x
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"  # ✅ Compatible with AWS provider <6.0.0
+
   cluster_name    = "flask-scalable-cluster"
   cluster_version = var.k8s_version
   vpc_id          = module.vpc.vpc_id
