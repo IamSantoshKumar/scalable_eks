fbc8950be8c49b3bca3d4ddad98f0ebeb96203e7
Santhoshkumar V <shanvsanthosh@gmail.com>
2025-07-19 10:32:20 +0530

    Initial commit

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 terraform {
-  required_version = ">= 1.1.0"
   required_providers {
-    aws = { source = "hashicorp/aws" version = ">= 5.0" }
-    helm = { source = "hashicorp/helm" version = ">= 2.0" }
-    kubernetes = { source = "hashicorp/kubernetes" version = ">= 2.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
   }
+
+  required_version = ">= 1.6.0"
 }
 
 provider "aws" {
