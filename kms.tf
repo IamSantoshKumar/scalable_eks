3ebb878a7499b7c7b5e1235de242191f2d231e13
Santhoshkumar V <shanvsanthosh@gmail.com>
2025-07-19 13:46:35 +0530

    fix

--- kms.tf
+++ kms.tf
@@ -1,4 +1,8 @@
+data "aws_kms_key" "cluster" {
+  key_id = "alias/eks/my-eks-cluster"
+}
+
 resource "aws_kms_alias" "this" {
   name          = "alias/eks/my-eks-cluster"
-  target_key_id = aws_kms_key.cluster.id
+  target_key_id = data.aws_kms_key.cluster.key_id
 }
