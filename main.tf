75847fdea5d236ca97a8875ee560352713bc1566
Santhoshkumar V <shanvsanthosh@gmail.com>
2025-07-19 12:46:38 +0530

    fix

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.40.0, < 6.0.0"
+      version = "~> 5.0"
     }
   }
 }
-
-
 
 provider "aws" {
   region = var.aws_region
@@ -16,44 +14,46 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"  # ✅ Tested version that works with AWS provider <6.0.0
+  version = "5.1.1"
 
-  name                 = "flask-scalable-vpc"
-  cidr                 = "10.0.0.0/16"
-  azs                  = ["${var.aws_region}a", "${var.aws_region}b"]
-  public_subnets       = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets      = ["10.0.3.0/24", "10.0.4.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-north-2a", "eu-north-2b", "eu-north-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
   tags = {
-    Terraform   = "true"
+    Terraform   = "true",
     Environment = "dev"
   }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.4"  # ✅ Compatible with AWS providers <6.0.0
+  version = "20.8.4"
 
-  cluster_name    = "flask-scalable-cluster"
-  cluster_version = var.k8s_version
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.29"
   vpc_id          = module.vpc.vpc_id
-  subnets         = module.vpc.private_subnets
-  manage_aws_auth = true
+  subnet_ids      = module.vpc.private_subnets
+
+  enable_irsa = true
 
   eks_managed_node_groups = {
-    flask_nodes = {
+    default = {
+      instance_types = ["t3.micro"]
       desired_size   = 2
       max_size       = 3
-      min_size       = 2
-      instance_types = ["t3.micro"]
-      subnet_ids     = module.vpc.private_subnets
+      min_size       = 1
     }
   }
 
   tags = {
-    Terraform   = "true"
     Environment = "dev"
+    Terraform   = "true"
   }
 }
