1cf4aa8f5439ce59ebb9517d4af3a9a67c9be57e
Santhoshkumar V <shanvsanthosh@gmail.com>
2025-07-19 10:21:07 +0530

    Initial commit

--- main.tf
+++ main.tf
@@ -41,5 +41,5 @@ module "eks" {
       subnet_ids     = module.vpc.private_subnets
     }
   }
-  tags = { Terraform = "true" Environment = "dev" }
+  tags = { Terraform = "true" Environment = "dev_" }
 }
